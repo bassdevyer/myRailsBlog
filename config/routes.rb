4af77ec48d21bb52e2104db433ac0a9b77f2c95d
miguetv17 <miguetv17@gmail.com>
2016-08-16 23:53:13 -0500

    Adding links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  # Rails provides a resources method which can be used to declare a standard REST resource.
+  resources :articles
 
   root 'welcome#index'
 end
